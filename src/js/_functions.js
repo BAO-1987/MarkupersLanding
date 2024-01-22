3b256b7fed81a855b153122e298a611e59e13710
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-01-22 16:24:11 +0200

    sssss

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,5 +1,4 @@
 import './functions/_pageHeight';
 import './functions/_headerHeight';
-import './functions/_headerScrollHide';
-import './functions/_accordion';
+// import './functions/_headerScrollHide';
 import './functions/_tabs';
--- src/js/functions/_tabs.js
+++ src/js/functions/_tabs.js
@@ -1,206 +1,91 @@
-// function initializeMultipleTabs() {
-//   const tabContainers = document.querySelectorAll(".tabs");
 
-//   tabContainers.forEach((container, index) => {
-//     const tabsList = container.querySelector(".tabs__nav");
-//     const tabButtons = tabsList.querySelectorAll(".tabs__link");
-//     const tabPanels = container.querySelectorAll(".tabs__panels > div");
+const tabsContainer = document.querySelector(".faq");
+const tabsList = tabsContainer.querySelector(".faq__list");
+const tabButtons = tabsList.querySelectorAll(".faq__link");
+const tabPanels = tabsContainer.querySelectorAll(".faq__panels > div");
 
-//     tabsList.setAttribute("role", "tablist");
+tabsList.setAttribute("role", "tablist");
 
-//     tabsList.querySelectorAll(".tabs__item").forEach((listitem) => {
-//       listitem.setAttribute("role", "presentation");
-//     });
+tabsList.querySelectorAll("li").forEach((listitem) => {
+  listitem.setAttribute("role", "presentation");
+});
 
-//     tabButtons.forEach((tab, tabIndex) => {
-//       tab.setAttribute("role", "tab");
-//       if (tabIndex === 0) {
-//         tab.setAttribute("aria-selected", "true");
-//       } else {
-//         tab.setAttribute("tabindex", "-1");
-//         tabPanels[tabIndex].setAttribute("hidden", "");
-//       }
-//     });
+tabButtons.forEach((tab, index) => {
+  tab.setAttribute("role", "tab");
+  tab.setAttribute("aria-selected", "false"); // Змінено значення на "false"
+  tab.setAttribute("tabindex", "-1");
+  tabPanels[index].setAttribute("hidden", "true"); // Змінено значення на "true"
+});
 
-//     tabPanels.forEach((panel) => {
-//       panel.setAttribute("role", "tabpanel");
-//       panel.setAttribute("tabindex", "0");
-//     });
 
-//     container.addEventListener("click", (e) => {
-//       const clickedTab = e.target.closest(".tabs__link");
-//       if (!clickedTab) return;
-//       e.preventDefault();
-//       switchTab(clickedTab);
-//     });
+tabPanels.forEach((panel) => {
+  panel.setAttribute("role", "tabpanel");
+  panel.setAttribute("tabindex", "0");
+});
 
-//     container.addEventListener("keydown", (e) => {
-//       switch (e.key) {
-//         case "ArrowLeft":
-//           moveLeft();
-//           break;
-//         case "ArrowRight":
-//           moveRight();
-//           break;
-//         case "Home":
-//           e.preventDefault();
-//           switchTab(tabButtons[0]);
-//           break;
-//         case "End":
-//           e.preventDefault();
-//           switchTab(tabButtons[tabButtons.length - 1]);
-//           break;
-//       }
-//     });
+tabsContainer.addEventListener("click", (e) => {
+  const clickedTab = e.target.closest(".faq__link");
+  if (!clickedTab) return;
+  e.preventDefault();
 
-//     function moveLeft() {
-//       const currentTab = document.activeElement;
-//       if (!currentTab.parentElement.previousElementSibling) {
-//         switchTab(tabButtons[tabButtons.length - 1]);
-//       } else {
-//         switchTab(
-//           currentTab.parentElement.previousElementSibling.querySelector(".tabs__link")
-//         );
-//       }
-//     }
+  switchTab(clickedTab);
+});
 
-//     function moveRight() {
-//       const currentTab = document.activeElement;
-//       if (!currentTab.parentElement.nextElementSibling) {
-//         switchTab(tabButtons[0]);
-//       } else {
-//         switchTab(currentTab.parentElement.nextElementSibling.querySelector(".tabs__link"));
-//       }
-//     }
-
-//     function switchTab(newTab) {
-//       const activePanelId = newTab.getAttribute("href");
-//       const activePanel = container.querySelector(activePanelId);
-
-//       tabButtons.forEach((button) => {
-//         button.setAttribute("aria-selected", false);
-//         button.setAttribute("tabindex", "-1");
-//       });
-
-//       tabPanels.forEach((panel) => {
-//         panel.setAttribute("hidden", true);
-//       });
-
-//       activePanel.removeAttribute("hidden", false);
-
-//       newTab.setAttribute("aria-selected", true);
-//       newTab.setAttribute("tabindex", "0");
-//       newTab.focus();
-//     }
-//   });
-// }
-
-// // Call the function to initialize multiple tab sets on the page
-// initializeMultipleTabs();
-
-function initializeMultipleTabs() {
-  // Select all elements with the class "tabs"
-  const tabContainers = document.querySelectorAll(".tabs");
-
-  // Iterate over each tab container
-  tabContainers.forEach((container, index) => {
-    // Find relevant elements within the tab container
-    const tabsList = container.querySelector(".tabs__nav");
-    const tabButtons = tabsList.querySelectorAll(".tabs__link");
-    const tabPanels = container.querySelectorAll(".tabs__panels > div");
-
-    // Set ARIA roles and attributes for accessibility
-    tabsList.setAttribute("role", "tablist");
-
-    tabsList.querySelectorAll("li").forEach((listitem) => {
-      listitem.setAttribute("role", "presentation");
-    });
-
-    // Make all tabs inactive and hide all panels initially
-    tabButtons.forEach((tab, tabIndex) => {
-      tab.setAttribute("role", "tab");
-      tab.setAttribute("aria-selected", "false");
-      tab.setAttribute("tabindex", "-1");
-      tabPanels[tabIndex].setAttribute("hidden", "");
-    });
-
-    tabPanels.forEach((panel) => {
-      panel.setAttribute("role", "tabpanel");
-      panel.setAttribute("tabindex", "0");
-    });
-
-    // Add event listeners for click and keydown events
-    container.addEventListener("click", (e) => {
-      const clickedTab = e.target.closest(".tabs__link");
-      if (!clickedTab) return;
+tabsContainer.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowLeft":
+      moveLeft();
+      break;
+    case "ArrowRight":
+      moveRight();
+      break;
+    case "Home":
       e.preventDefault();
-      switchTab(clickedTab);
-    });
+      switchTab(tabButtons[0]);
+      break;
+    case "End":
+      e.preventDefault();
+      switchTab(tabButtons[tabButtons.length - 1]);
+      break;
+  }
+});
 
-    container.addEventListener("keydown", (e) => {
-      switch (e.key) {
-        case "ArrowLeft":
-          moveLeft();
-          break;
-        case "ArrowRight":
-          moveRight();
-          break;
-        case "Home":
-          e.preventDefault();
-          switchTab(tabButtons[0]);
-          break;
-        case "End":
-          e.preventDefault();
-          switchTab(tabButtons[tabButtons.length - 1]);
-          break;
-      }
-    });
-
-    // Function to move to the previous tab
-    function moveLeft() {
-      const currentTab = document.activeElement;
-      if (!currentTab.parentElement.previousElementSibling) {
-        switchTab(tabButtons[tabButtons.length - 1]);
-      } else {
-        switchTab(
-          currentTab.parentElement.previousElementSibling.querySelector(".tabs__link")
-        );
-      }
-    }
-
-    // Function to move to the next tab
-    function moveRight() {
-      const currentTab = document.activeElement;
-      if (!currentTab.parentElement.nextElementSibling) {
-        switchTab(tabButtons[0]);
-      } else {
-        switchTab(currentTab.parentElement.nextElementSibling.querySelector(".tabs__link"));
-      }
-    }
-
-    // Function to switch to a new tab
-    function switchTab(newTab) {
-      const activePanelId = newTab.getAttribute("href");
-      const activePanel = container.querySelector(activePanelId);
-
-      // Update ARIA attributes and visibility
-      tabButtons.forEach((button) => {
-        button.setAttribute("aria-selected", false);
-        button.setAttribute("tabindex", "-1");
-      });
-
-      tabPanels.forEach((panel) => {
-        panel.setAttribute("hidden", true);
-      });
-
-      activePanel.removeAttribute("hidden", false);
-
-      newTab.setAttribute("aria-selected", true);
-      newTab.setAttribute("tabindex", "0");
-      newTab.focus();
-    }
-  });
+function moveLeft() {
+  const currentTab = document.activeElement;
+  if (!currentTab.parentElement.previousElementSibling) {
+    switchTab(tabButtons[tabButtons.length - 1]);
+  } else {
+    switchTab(
+      currentTab.parentElement.previousElementSibling.querySelector("a")
+    );
+  }
 }
 
-// Call the function to initialize multiple tab sets on the page
-initializeMultipleTabs();
+function moveRight() {
+  const currentTab = document.activeElement;
+  if (!currentTab.parentElement.nextElementSibling) {
+    switchTab(tabButtons[0]);
+  } else {
+    switchTab(currentTab.parentElement.nextElementSibling.querySelector("a"));
+  }
+}
+
+function switchTab(newTab) {
+  const activePanelId = newTab.getAttribute("href");
+  const activePanel = tabsContainer.querySelector(activePanelId);
+
+  tabButtons.forEach((button) => {
+    button.setAttribute("aria-selected", false);
+    button.setAttribute("tabindex", "-1");
+  });
+
+  tabPanels.forEach((panel) => {
+    panel.setAttribute("hidden", true);
+  });
+
+  activePanel.removeAttribute("hidden", false);
+
+  newTab.setAttribute("aria-selected", true);
+  newTab.setAttribute("tabindex", "0");
+  newTab.focus();
+}
