c0ea5387c62e02d4bf8a505bc7f6338ef85192bf
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-02-03 15:28:44 +0200

    add dialog

--- src/js/components/_animation.js
+++ src/js/components/_animation.js
@@ -4,14 +4,14 @@ ScrollReveal({
 });
 
 
-ScrollReveal().reveal(".text, .delivery__content, .slider, .app__content,  .catalog__inner, .filter, .sort, .product-info, .tabs, .recent__content", {
+ScrollReveal().reveal(".text, .portfolio-home, .reviews-home, .contacts-home", {
   duration: 1200,
   distance: "20px",
   delay: "100",
   origin: "top",
 });
 
-ScrollReveal().reveal(".services-home__list> li, .stages__list> li", {
+ScrollReveal().reveal(".services-home__list> li, .stages__list> li, .faq__list> li", {
   duration: 500,
   distance: "20px",
   delay: "100",
@@ -21,16 +21,16 @@ ScrollReveal().reveal(".services-home__list> li, .stages__list> li", {
 });
 
 
-ScrollReveal().reveal(".hero__image, .delivery__image, .app__image", {
-  duration: 1000,
-  distance: "20px",
-  delay: "100",
-  origin: "right",
-});
+// ScrollReveal().reveal(".hero__image, .delivery__image, .app__image", {
+//   duration: 1000,
+//   distance: "20px",
+//   delay: "100",
+//   origin: "right",
+// });
 
-ScrollReveal().reveal(".delivery__image", {
-  origin: "left",
-});
+// ScrollReveal().reveal(".delivery__image", {
+//   origin: "left",
+// });
 
 // ScrollReveal().reveal(".title", {
 //   duration: 1200,
@@ -41,10 +41,10 @@ ScrollReveal().reveal(".delivery__image", {
 
 
 
-ScrollReveal().reveal(".footer__top, .categories__btn, .pagination", {
-  duration: 800,
-  distance: "25px",
-  delay: "100",
-  origin: "bottom",
-});
+// ScrollReveal().reveal(".btn", {
+//   duration: 300,
+//   distance: "25px",
+//   delay: "100",
+//   origin: "top",
+// });
 
