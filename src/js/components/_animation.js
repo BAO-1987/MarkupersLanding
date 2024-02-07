abc5155196b2d22b3bb127a2638591176b57e658
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-02-07 23:07:35 +0200

    dss animation

--- src/js/components/_animation.js
+++ src/js/components/_animation.js
@@ -3,6 +3,31 @@ ScrollReveal({
   easing: "ease",
 });
 
+ScrollReveal().reveal(".header", {
+  duration: 800,
+  distance: "100%",
+  delay: "800",
+  origin: "top",
+  mobile: false,
+});
+
+ScrollReveal().reveal(".hero__title", {
+  duration: 700,
+  distance: "20px",
+  easing: "ease",
+  delay: "150",
+  origin: "top",
+});
+
+ScrollReveal().reveal(".hero__btn", {
+  duration: 800,
+  distance: "30px",
+  delay: "600",
+  origin: "bottom",
+  mobile: false,
+});
+
+
 
 ScrollReveal().reveal(".text, .portfolio, .reviews, .contacts, .stages__list", {
   duration: 1200,
@@ -48,8 +73,3 @@ ScrollReveal().reveal(".faq__list> li", {
 //   delay: "100",
 //   origin: "top",
 // });
-
-
-
-
-
