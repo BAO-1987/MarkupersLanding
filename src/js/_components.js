45f76c1dec268fc58f991085e01aee6f167d871d
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-02-07 13:35:58 +0200

    add canvas

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,7 +2,7 @@ import './components/_cardHover';
 import './components/_portfolioSlider';
 import './components/_reviewsSlider';
 import './components/_aboutSlider';
-// import './components/_canvasAnimation';
+import './components/_canvas';
 import './components/_burger';
 import './components/_animation';
 import './components/_modal';
